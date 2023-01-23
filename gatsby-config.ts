628617792b1c40e226fb760226b94568f7af6dc8
Punptts <punpun.ptts@gmail.com>
2023-01-23 15:23:19 +0700

    add gh-pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,11 @@
 import type { GatsbyConfig } from "gatsby";
-
+export const pathPrefix = "/pantitas-website";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `pantitas-website`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
